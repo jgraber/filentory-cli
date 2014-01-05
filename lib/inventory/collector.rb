9da5637e80b24f9cc02d108f414aac1167f2d75c
Johnny Graber <jg@jgraber.ch>
2014-01-05 20:48:26 +0100

    add logger to Collector

--- lib/inventory/collector.rb
+++ lib/inventory/collector.rb
@@ -1,13 +1,19 @@
 require 'find'
 require 'digest'
+#require 'methadone/cli_logger'
+require 'methadone'
 
 class Collector
+  include Methadone::CLILogging
 
   def collect(path)
     result = Array.new
 
     Find.find(path) do |file|
-      next if File.directory?(file)
+      if File.directory?(file)
+        info "skipping #{file}"
+        next
+      end
       result << extract_file_infos(file)
     end
 
@@ -20,11 +26,14 @@ class Collector
     directory_name = File.dirname(file_path)
     file_name = File.basename(file_path)
 
-    entry = FileEntry.new(directory_name, file_name)
-    entry.last_modified= File.atime(file_path)
-    entry.size = File.size(file_path)
-    entry.checksum = Digest::SHA2.file(file_path).hexdigest
-
+    begin
+      entry = FileEntry.new(directory_name, file_name)
+      entry.last_modified= File.atime(file_path)
+      entry.size = File.size(file_path)
+      entry.checksum = Digest::SHA2.file(file_path).hexdigest
+    rescue => file_error
+      error ("error with file '#{file_path}': #{file_error}")
+    end
     entry
   end
 end
