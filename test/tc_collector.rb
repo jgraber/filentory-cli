debef3b8fc71f557716bd3baee85abb5401114bc
Johnny Graber <jg@jgraber.ch>
2014-01-07 23:01:22 +0100

    prepare testdata for exif metadata

--- test/tc_collector.rb
+++ test/tc_collector.rb
@@ -8,7 +8,7 @@ class TestCollector < Test::Unit::TestCase
     col = Collector.new
     result = col.collect(File.dirname(__FILE__)+"/integration/data/")
 
-    assert_equal(2, result.length)
+    assert_equal(3, result.length)
     assert_equal("bf65dac6b214f362fc6a8b151c9f44668fed8460bbe8204f215e82332a295939", result[0].checksum)
     assert_equal(".", result[0].path)
   end
