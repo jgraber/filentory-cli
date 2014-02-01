6e8c8cfbbacd7ae88bad122dd4ed6ebdd1c8072c
Johnny Graber <jg@jgraber.ch>
2014-02-01 15:27:39 +0100

    start with rdoc

--- lib/filentory/fileentry.rb
+++ lib/filentory/fileentry.rb
@@ -1,3 +1,5 @@
+# FileEntry is used as a simple container to store information 
+# about the file like metadata, size or checksum.
 class FileEntry
   attr_accessor :path, :name, :checksum, :size, :metadata
 
@@ -6,14 +8,18 @@ class FileEntry
     @name = name
   end
 
+  # Set the last_modified date. It uses the date.strftime method 
+  # to convert it in a ISO 8601 format to be radable in JSON.
   def last_modified=(date)
     @last_modified = date.strftime("%FT%T%:z")
   end
 
+  # Get the last_modified date 
   def last_modified
     DateTime.iso8601(@last_modified)
   end
 
+  # Overrides to_s to get a human readable output.
   def to_s
     @path + @name + " (" + @size.to_s + ") - " + @last_modified
   end
