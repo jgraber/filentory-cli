f2adbcaa46a4d5155b8bac2a12e727fae5b01358
Johnny Graber <jg@jgraber.ch>
2014-12-13 12:00:27 +0100

    use delta to compare floats (Linux != Mac)

--- test/tc_videoextractor.rb
+++ test/tc_videoextractor.rb
@@ -8,8 +8,8 @@ class TestVideoExtractor < Minitest::Test
   def test_can_extract_metadata
     extractor = VideoExtractor.new
     metadata = extractor.metadata_for_file(File.dirname(__FILE__)+"/integration/data/video.mov")
-
-    assert_equal(2.18, metadata["duration"])
+    
+    assert_in_delta(2.18, metadata["duration"], 0.05)
     assert_equal("568x320", metadata["resolution"])
   end
 
